1a15f6111222cc41b67c06933b56daa739db2d46
Bram Borggreve <borggreve@gmail.com>
2018-01-11 21:09:51 +0700

    Lesson 1.6 A - Create the Posts Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './ui/components/layout/layout.component'
 
 const routes: Routes = [
-  { path: '', component: LayoutComponent, children: [] },
+  { path: '', component: LayoutComponent, children: [
+    { path: '', redirectTo: '/posts', pathMatch: 'full'},
+    { path: 'posts', loadChildren: './posts/posts.module#PostsModule' },
+  ] },
 ];
 
 @NgModule({
---
+++ src/app/posts/posts.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PostsRoutingModule } from './posts-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    PostsRoutingModule
+  ],
+  declarations: []
+})
+export class PostsModule { }
