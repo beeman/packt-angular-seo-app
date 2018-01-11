b89fc217844d2747b5abf942bd9ddeef2a196def
Bram Borggreve <borggreve@gmail.com>
2018-01-12 04:44:39 +0700

    Lesson 2.1 - Generate server app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
     AppRoutingModule,
     UiModule,
