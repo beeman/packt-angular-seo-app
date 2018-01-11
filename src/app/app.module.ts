f4b86c9fa95c408598ba91436a6b38fe5659d8c7
Bram Borggreve <borggreve@gmail.com>
2018-01-11 19:07:31 +0700

    Lesson 1.5 A - Create the UI Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { UiModule } from './ui/ui.module'
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UiModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
